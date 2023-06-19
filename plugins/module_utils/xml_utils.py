4504f077b243636f64c6cb657c0fa8cb04e58b51
Fabio Bertagna <bertagna@puzzle.ch>
2023-06-19 20:28:06 +0200

    Enforce single root xml

--- plugins/module_utils/xml_utils.py
+++ plugins/module_utils/xml_utils.py
@@ -9,13 +9,24 @@ from xml.etree.ElementTree import Element
 __metaclass__ = type
 
 
+class XMLUtilsUnsupportedInputFormatError(Exception):
+    pass
+
+
 def dict_to_etree(input_dict: dict) -> Element:
     """
     Generates a python dictionary to an ElementTree.Element.
     :param input_dict: dictionary with input data
     :return: generated ElementTree.Element
     """
-    tag_name = list(input_dict.keys())[0]
+    input_dict_keys = list(input_dict.keys())
+
+    if len(input_dict_keys) > 1:
+        raise XMLUtilsUnsupportedInputFormatError(
+            "xml_utils only support dictionaries using a single root entry."
+        )
+
+    tag_name = input_dict_keys[0]
     value = input_dict[tag_name]
     new_element = Element(tag_name)
     new_element.text = value
