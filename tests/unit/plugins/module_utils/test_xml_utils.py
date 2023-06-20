19e736e801f7762843bac5e50957b531b477e787
Fabio Bertagna <bertagna@puzzle.ch>
2023-06-20 11:37:42 +0200

    xml_utils test primitives and simple dicts

--- tests/unit/plugins/module_utils/test_xml_utils.py
+++ tests/unit/plugins/module_utils/test_xml_utils.py
@@ -7,54 +7,51 @@ from __future__ import (absolute_import, division, print_function)
 
 __metaclass__ = type
 
-from unittest import mock
+from typing import Union, Optional
 from xml.etree.ElementTree import Element
 
 import pytest
 from ansible_collections.puzzle.opnsense.plugins.module_utils import xml_utils
 
 
-@pytest.mark.parametrize("input_dict", [
-    {"test": 1},
-    {"test": "some_string"},
-    {"test": None}
+@pytest.mark.parametrize("input_data", [
+    1,
+    "foo",
+    None
 ])
-def test_dict_to_etree__simple_dict_with_value(input_dict: dict) -> None:
+def test_dict_to_etree__primitive_values(input_data: Optional[Union[int, str]]) -> None:
     """
     Given a simple input dictionary, check that the xml tag takes the dict key
     value and the xml inner text is set as the dict value. E.g.:
     Given: {"test":1}
     Expected XML: <test>1</test>
     """
-    output_etree: Element = xml_utils.dict_to_etree(input_dict)
+    test_tag: str = "test"
+    output_etree: list[Element] = xml_utils.dict_to_etree(test_tag, input_data)
 
-    assert output_etree.tag == list(input_dict.keys())[0]
-    assert output_etree.text == input_dict[list(input_dict.keys())[0]]
+    assert len(output_etree) == 1
+    assert output_etree[0].tag == test_tag
+    assert output_etree[0].text == input_data
 
 
-def test_dict_to_etree__enforce_single_root_dict() -> None:
+@pytest.mark.parametrize("input_data", [
+    {"foo": 1},
+    {"foo": "bar"},
+    {"foo": 1, "bar": None},
+])
+def test_dict_to_etree__simple_dicts(input_data: dict) -> None:
     """
     Given a simple input dictionary, check that the xml tag takes the dict key
     value and the xml inner text is set as the dict value. E.g.:
     Given: {"test":1}
     Expected XML: <test>1</test>
     """
-    input_dict: dict = {"test_1": 1, "test_2": 2}
-    with pytest.raises(xml_utils.XMLUtilsUnsupportedInputFormatError):
-        output_etree: Element = xml_utils.dict_to_etree(input_dict)
-
-
-@pytest.mark.parametrize("input_dict", [
-    {"test": 1},
-    {"test": "some_string"},
-    {"test": {"child": "some_string"}},
-    {"test": [{"child_1": 1}]}
-])
-def test_dict_to_etree__invoke_helper_function_when_child_is_list(input_dict) -> None:
-    with mock.patch("ansible_collections.puzzle.opnsense.plugins.module_utils.xml_utils._parse_children_from_dict")\
-            as mock_parse_children_from_dict:
-        output_etree: Element = xml_utils.dict_to_etree(input_dict)
-        if isinstance(input_dict.get("test"), list) or isinstance(input_dict.get("test"), dict):
-            mock_parse_children_from_dict.assert_called_once()
-        else:
-            mock_parse_children_from_dict.assert_not_called()
+    test_tag: str = "test"
+    output_etree: list[Element] = xml_utils.dict_to_etree(test_tag, input_data)
+    output_children: list[Element] = list(output_etree[0])
+    assert len(output_etree) == 1
+    assert output_etree[0].tag == test_tag
+    assert output_etree[0].text is None
+    assert len(output_children) == len(input_data)
+    for out_child in output_children:
+        assert out_child.tag in list(input_data.keys())
