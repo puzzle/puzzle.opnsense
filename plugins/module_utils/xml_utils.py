2be9484a008d14ff59769f4b0965d164466065a6
Fabio Bertagna <bertagna@puzzle.ch>
2023-06-20 13:09:52 +0200

    xml_utils add helper functions

--- plugins/module_utils/xml_utils.py
+++ plugins/module_utils/xml_utils.py
@@ -2,6 +2,7 @@
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 """Utilities for XML operations."""
+
 from __future__ import (absolute_import, division, print_function)
 
 from typing import Union, Optional
@@ -12,50 +13,94 @@ __metaclass__ = type
 
 def dict_to_etree(tag: str, data: Optional[Union[int, str, list, dict]]) -> list[Element]:
     """
-    Generates a python dictionary to an ElementTree.Element.
+    Converts a Python dictionary to an ElementTree.Element structure.
 
-    :param tag: root element tag
-    :param data: root element children structure data
-    :return: generated ElementTree.Element
+    :param tag: The root element tag.
+    :param data: The root element children structure data.
+    :return: The generated list of ElementTree.Element.
     """
-    if isinstance(data, int) or isinstance(data, str) or data is None:
-        new_element: Element = Element(tag)
-        new_element.text = data
-        return [new_element]
+    if isinstance(data, (int, str, type(None))):
+        return [_create_element(tag, data)]
 
     elif isinstance(data, dict):
-        new_element: Element = Element(tag)
-        for key, val in data.items():
-            child_elements: list[Element] = dict_to_etree(key, val)
-            for child in child_elements:
-                new_element.append(child)
-
-        return [new_element]
+        return [_create_element_from_dict(tag, data)]
 
     elif isinstance(data, list):
-        new_elements: list[Element] = []
-        root: Optional[Element] = None
+        return _process_list(tag, data)
 
-        for data_item in data:
-            # primitive list elements
-            if isinstance(data_item, int) or isinstance(data_item, str) or data_item is None:
-                new_items: list[Element] = dict_to_etree(tag, data_item)
 
-                if len(new_items) != 1:
-                    raise AssertionError("Primitive or None type must return only a single element")
+def _create_element(tag: str, data: Optional[Union[int, str]]) -> Element:
+    """
+    Creates an ElementTree.Element with the given tag and optional text content.
 
-                for item in new_items:
-                    new_elements.append(item)
+    :param tag: The element tag.
+    :param data: The optional text content of the element.
+    :return: The created ElementTree.Element.
+    """
+    new_element: Element = Element(tag)
+    new_element.text = data
+    return new_element
 
-            # dict list elements
-            elif isinstance(data_item, dict):
-                if root is None:
-                    root = Element(tag)
-                for key, val in data_item.items():
-                    child_elements: list[Element] = dict_to_etree(key, val)
-                    for child in child_elements:
-                        root.append(child)
 
-        if root is not None:
-            new_elements.append(root)
-        return new_elements
+def _create_element_from_dict(tag: str, data: dict) -> Element:
+    """
+    Creates an ElementTree.Element from a dictionary by recursively converting its key-value pairs.
+
+    :param tag: The tag for the new element.
+    :param data: The dictionary containing key-value pairs.
+    :return: The created ElementTree.Element.
+    """
+    new_element: Element = Element(tag)
+    for key, val in data.items():
+        child_elements: list[Element] = dict_to_etree(key, val)
+        for child in child_elements:
+            new_element.append(child)
+    return new_element
+
+
+def _process_list(tag: str, data: list) -> list[Element]:
+    """
+    Processes a list by iterating over its elements and converting them to ElementTree.Element.
+
+    :param tag: The tag for the new elements.
+    :param data: The list of elements to process.
+    :return: The list of generated ElementTree.Element.
+    """
+    new_elements: list[Element] = []
+    root: Optional[Element] = None
+
+    for data_item in data:
+        if isinstance(data_item, (int, str, type(None))):
+            new_items: list[Element] = dict_to_etree(tag, data_item)
+
+            if len(new_items) != 1:
+                raise AssertionError("Primitive or None type must return only a single element")
+
+            new_elements.extend(new_items)
+
+        elif isinstance(data_item, dict):
+            root = _process_dict_list(tag, data_item, root)
+
+    if root is not None:
+        new_elements.append(root)
+
+    return new_elements
+
+
+def _process_dict_list(tag: str, data_item: dict, root: Optional[Element]) -> Optional[Element]:
+    """
+    Processes a dictionary within a list, converting its key-value pairs to ElementTree.Element.
+
+    :param tag: The tag for the new elements.
+    :param data_item: The dictionary containing key-value pairs.
+    :param root: The root element to append child elements.
+    :return: The updated root element.
+    """
+    if root is None:
+        root = Element(tag)
+
+    for key, val in data_item.items():
+        child_elements: list[Element] = dict_to_etree(key, val)
+        root.extend(child_elements)
+
+    return root
--- tests/unit/plugins/module_utils/test_xml_utils.py
+++ tests/unit/plugins/module_utils/test_xml_utils.py
@@ -21,10 +21,14 @@ from ansible_collections.puzzle.opnsense.plugins.module_utils import xml_utils
 ])
 def test_dict_to_etree__primitive_values(input_data: Optional[Union[int, str]]) -> None:
     """
-    Given a simple input dictionary, check that the xml tag takes the dict key
-    value and the xml inner text is set as the dict value. E.g.:
-    Given: {"test":1}
-    Expected XML: <test>1</test>
+    Test converting a primitive value to an ElementTree.Element.
+
+    Given a primitive input value, the function should create an ElementTree.Element with the given tag
+    and set the text content of the element to the input value.
+
+    For example:
+    - Input: 1
+    - Expected XML: <test>1</test>
     """
     test_tag: str = "test"
     output_etree: list[Element] = xml_utils.dict_to_etree(test_tag, input_data)
@@ -41,14 +45,19 @@ def test_dict_to_etree__primitive_values(input_data: Optional[Union[int, str]]) 
 ])
 def test_dict_to_etree__tags_on_simple_dicts(input_data: dict) -> None:
     """
-    Given a simple input dictionary, check that the xml tag takes the dict key
-    value and the xml inner text is set as the dict value. E.g.:
-    Given: {"test":1}
-    Expected XML: <test>1</test>
+    Test converting a simple dictionary to an ElementTree.Element.
+
+    Given a simple input dictionary, the function should create an ElementTree.Element with the given tag
+    and add child elements for each key-value pair in the dictionary.
+
+    For example:
+    - Input: {"test": 1}
+    - Expected XML: <test>1</test>
     """
     test_tag: str = "test"
     output_etree: list[Element] = xml_utils.dict_to_etree(test_tag, input_data)
     output_children: list[Element] = list(output_etree[0])
+
     assert len(output_etree) == 1
     assert output_etree[0].tag == test_tag
     assert output_etree[0].text is None
@@ -59,18 +68,22 @@ def test_dict_to_etree__tags_on_simple_dicts(input_data: dict) -> None:
 
 def test_dict_to_etree__dict_recursion() -> None:
     """
-    Test that nested dict create elements recursively.
-    {
-        "foo" : {                   <foo>
-            "bar" : {                   <bar>
-                "test" : 1,                 <test>1</test>
-                "john" : {                  <john>
-                    "doe" : 1   =>              <doe>1</doe>
-                }                           </john>
-            }                           </bar>
-        }                           </foo>
-    }
-    :return:
+    Test converting a nested dictionary to an ElementTree.Element.
+
+    Given a nested dictionary, the function should create a hierarchical structure of ElementTree.Elements
+    with the corresponding tags and values.
+
+    For example:
+    - Input: {"bar": {"test": 1, "john": {"doe": 1}}}
+    - Expected XML:
+        <foo>
+            <bar>
+                <test>1</test>
+                <john>
+                    <doe>1</doe>
+                </john>
+            </bar>
+        </foo>
     """
     test_tag: str = "foo"
     input_dict: dict = {"bar": {"test": 1, "john": {"doe": 1}}}
@@ -102,12 +115,18 @@ def test_dict_to_etree__dict_recursion() -> None:
 ])
 def test_dict_to_etree__primitive_list(input_data: list) -> None:
     """
-    A primitive input list (int/str) must return multiple elements.
+    Test converting a primitive list to multiple ElementTree.Elements.
 
-                            <foo>1</foo>
-    { "foo" : [1,2,3] } =>  <foo>2</foo>
-                            <foo>3</foo>
+    Given a list of primitive values (int/str), the function should create multiple ElementTree.Elements,
+    each with the given tag and corresponding value.
 
+    For example:
+    - Input: [1, 2, 3, 4]
+    - Expected XML:
+        <foo>1</foo>
+        <foo>2</foo>
+        <foo>3</foo>
+        <foo>4</foo>
     """
     test_tag: str = "foo"
 
@@ -121,14 +140,19 @@ def test_dict_to_etree__primitive_list(input_data: list) -> None:
 
 def test_dict_to_etree__list_with_dicts_or_sub_lists() -> None:
     """
-    An input list with dict elements must return multiple elements.
+    Test converting a list with dictionaries or sub-lists to multiple ElementTree.Elements.
 
-    {                                           <foo>
-        "foo" : [                                   <bar>1</bar>
-            {"bar":1},{"bar":2},{"bar":3}   =>      <bar>2</bar>
-        ]                                           <bar>3</bar>
-    }                                           </foo>
+    Given a list containing dictionaries or sub-lists, the function should create multiple ElementTree.Elements,
+    each representing the corresponding dictionary or sub-list.
 
+    For example:
+    - Input: [{"bar": 1}, {"bar": 2}, {"bar": 3}]
+    - Expected XML:
+        <foo>
+            <bar>1</bar>
+            <bar>2</bar>
+            <bar>3</bar>
+        </foo>
     """
     test_tag: str = "foo"
     input_list: list[dict] = [{"bar": 1}, {"bar": 2}, {"bar": 3}]
@@ -143,15 +167,19 @@ def test_dict_to_etree__list_with_dicts_or_sub_lists() -> None:
 
 def test_dict_to_etree__list_with_dicts_dict_flattening() -> None:
     """
-    An input list with dict of which some could contain multiple items,
-    must return a flattened xml structure.
+    Test converting a list with dictionaries, some of which contain multiple items, to flattened ElementTree.Elements.
 
-    {                                           <foo>
-        "foo" : [                                   <bar>1</bar>
-            {"bar":1},{"bob":2, "cat":3}    =>      <bob>2</bob>
-        ]                                           <cat>3</cat>
-    }                                           </foo>
+    Given a list containing dictionaries, some of which may have multiple items, the function should create a flattened
+    structure of ElementTree.Elements.
 
+    For example:
+    - Input: [{"bar": 1}, {"bar": 2}, {"bar": 3}]
+    - Expected XML:
+        <foo>
+            <bar>1</bar>
+            <bar>2</bar>
+            <bar>3</bar>
+        </foo>
     """
     test_tag: str = "foo"
     input_list: list[dict] = [{"bar": 1}, {"bar": 2}, {"bar": 3}]
