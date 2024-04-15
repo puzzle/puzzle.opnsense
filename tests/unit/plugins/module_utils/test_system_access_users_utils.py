07ce948f505b6fd170a934db6adb0a42ef8958a1
KiLLuuuhh <kilian.soltermann@gmx.ch>
2024-04-15 09:25:14 +0200

    excluded tests from pylint

--- tests/unit/plugins/module_utils/test_system_access_users_utils.py
+++ tests/unit/plugins/module_utils/test_system_access_users_utils.py
@@ -1,5 +1,6 @@
 # Copyright: (c) 2024, Puzzle ITC, Kilian Soltermann <soltermann@puzzle.ch>
 #  GNU General Public License v3.0+ (see LICENSE or https://www.gnu.org/licenses/gpl-3.0.txt)
+# pylint: skip-file
 import os
 from tempfile import NamedTemporaryFile
 from unittest.mock import patch, MagicMock
