c5396ca28016ad300f37f7f667413eef6e9abd13
KiLLuuuhh <kilian.soltermann@gmx.ch>
2024-04-17 23:00:20 +0200

    added fail_json exceptions

--- plugins/modules/interfaces_assignments.py
+++ plugins/modules/interfaces_assignments.py
@@ -24,7 +24,7 @@ options:
     description:
       - "Technical identifier of the interface, used by hasync for example"
     type: str
-    required: true
+    required: False
   device:
     description:
       - Physical Device Name eg. vtnet0, ipsec1000 etc,.
@@ -82,6 +82,9 @@ from ansible.module_utils.basic import AnsibleModule
 from ansible_collections.puzzle.opnsense.plugins.module_utils.interfaces_assignments_utils import (
     InterfacesSet,
     InterfaceAssignment,
+    OPNSenseInterfaceNotFoundError,
+    OPNSenseDeviceNotFoundError,
+    OPNSenseDeviceAlreadyAssignedError,
 )
 
 
@@ -112,29 +115,45 @@ def main():
         "diff": None,
     }
 
-    interface_assignment = InterfaceAssignment.from_ansible_module_params(module.params)
+    try:
+        interface_assignment = InterfaceAssignment.from_ansible_module_params(
+            module.params
+        )
 
-    with InterfacesSet() as interfaces_set:
+        with InterfacesSet() as interfaces_set:
 
-        interfaces_set.update(interface_assignment)
+            interfaces_set.update(interface_assignment)
 
-        if interfaces_set.changed:
-            result["diff"] = interfaces_set.diff
-            result["changed"] = True
+            if interfaces_set.changed:
+                result["diff"] = interfaces_set.diff
+                result["changed"] = True
 
-        if interfaces_set.changed and not module.check_mode:
-            interfaces_set.save()
-            result["opnsense_configure_output"] = interfaces_set.apply_settings()
+            if interfaces_set.changed and not module.check_mode:
+                interfaces_set.save()
+                result["opnsense_configure_output"] = interfaces_set.apply_settings()
 
-            for cmd_result in result["opnsense_configure_output"]:
-                if cmd_result["rc"] != 0:
-                    module.fail_json(
-                        msg="Apply of the OPNsense settings failed",
-                        details=cmd_result,
-                    )
+                for cmd_result in result["opnsense_configure_output"]:
+                    if cmd_result["rc"] != 0:
+                        module.fail_json(
+                            msg="Apply of the OPNsense settings failed",
+                            details=cmd_result,
+                        )
 
-    # Return results
-    module.exit_json(**result)
+        # Return results
+        module.exit_json(**result)
+
+    except (
+        OPNSenseInterfaceNotFoundError
+    ) as opnsense_interface_not_found_error_error_message:
+        module.fail_json(msg=str(opnsense_interface_not_found_error_error_message))
+
+    except OPNSenseDeviceNotFoundError as opnsense_device_not_found_error_error_message:
+        module.fail_json(msg=str(opnsense_device_not_found_error_error_message))
+
+    except (
+        OPNSenseDeviceAlreadyAssignedError
+    ) as opnsense_device_already_assigned_error_message:
+        module.fail_json(msg=str(opnsense_device_already_assigned_error_message))
 
 
 if __name__ == "__main__":
