6eb044cbe66b2c61267a2aaeb9007a6be472fb05
KiLLuuuhh <kilian.soltermann@gmx.ch>
2024-05-24 17:28:01 +0200

    added linter exclusion

--- plugins/module_utils/system_access_users_utils.py
+++ plugins/module_utils/system_access_users_utils.py
@@ -1,5 +1,6 @@
 #  Copyright: (c) 2024, Puzzle ITC, Kilian Soltermann <soltermann@puzzle.ch>
 #  GNU General Public License v3.0+ (see LICENSE or https://www.gnu.org/licenses/gpl-3.0.txt)
+# pylint: disable=too-many-lines
 
 """
 This module manages user and group configurations within an OPNsense system. It provides
