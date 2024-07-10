c7c041bdd3a94981ab242ade2ed9655841f802ab
KiLLuuuhh <kilian.soltermann@gmx.ch>
2024-07-10 09:25:38 +0200

    first working draft

--- plugins/module_utils/firewall_alias_utils.py
+++ plugins/module_utils/firewall_alias_utils.py
@@ -4,6 +4,7 @@
 """
 Utilities for alias related operations.
 """
+import uuid
 from typing import List, Optional
 
 from xml.etree.ElementTree import Element, ElementTree
@@ -49,7 +50,7 @@ class FirewallAlias:
 
     def __init__(self, **kwargs):
         # set default attributes
-        self.uuid: Optional[str] = None
+        self.uuid: Optional[str] = kwargs.get("uuid", str(uuid.uuid4()))
         self.enabled: bool = True
         self.proto: Optional[IPProtocol] = None
         self.counters: Optional[bool] = False
@@ -62,6 +63,8 @@ class FirewallAlias:
         for key, value in kwargs.items():
             setattr(self, key, value)
 
+        self.__post_init__()
+
     def __post_init__(self):
         # Manually define the fields and their expected types
         enum_fields = {"type": FirewallAliasType}
@@ -73,6 +76,11 @@ class FirewallAlias:
             if isinstance(value, str) and issubclass(field_type, ListEnum):
                 # Convert string to ListEnum
                 setattr(self, field_name, field_type.from_string(value))
+
+    def __eq__(self, other):
+        if isinstance(other, FirewallAlias):
+            return self.__dict__ == other.__dict__
+        return False
 
     @staticmethod
     def from_xml(element: Element) -> "FirewallAlias":
@@ -90,7 +98,7 @@ class FirewallAlias:
         )
 
         # process attribute content to a list
-        if firewall_alias_dict.get("content"):
+        if isinstance(firewall_alias_dict.get("content"), list):
             firewall_alias_dict["content"] = [
                 line.strip()
                 for line in firewall_alias_dict["content"].splitlines()
@@ -108,8 +116,7 @@ class FirewallAlias:
         firewall_alias_dict: dict = {
             "enabled": params.get("enabled"),
             "name": params.get("name"),
-            "type": FirewallAliasType(params.get("type")),
-            "categories": params.get("categories"),
+            "type": params.get("type"),
             "content": params.get("content"),
             "counters": params.get("statistics"),
             "description": params.get("description"),
@@ -132,14 +139,26 @@ class FirewallAlias:
         firewall_alias_dict: dict = self.__dict__.copy()
         del firewall_alias_dict["uuid"]
 
+        for alias_key, alias_val in firewall_alias_dict.copy().items():
+
+            if alias_key in ["enabled", "counters"]:
+                firewall_alias_dict[alias_key] = "0" if alias_val is False else "1"
+                continue
+
+            if isinstance(alias_val, bool):
+                firewall_alias_dict[alias_key] = "0" if alias_val is False else "1"
+                continue
+
+            if issubclass(type(alias_val), ListEnum):
+                firewall_alias_dict[alias_key] = alias_val.value
+                continue
+
         # Handle content field if it is a list
         if isinstance(firewall_alias_dict.get("content"), list):
             firewall_alias_dict["content"] = (
                 "\n"
-                + "\n".join(
-                    [f"            {item}" for item in firewall_alias_dict["content"]]
-                )
-                + "\n            "
+                + "\n".join([f"{item}" for item in firewall_alias_dict["content"]])
+                + "\n"
             )
 
         element: Element = xml_utils.dict_to_etree("alias", firewall_alias_dict)[0]
@@ -173,3 +192,69 @@ class FirewallAliasSet(OPNsenseModuleConfig):
         element_tree_alias: Element = self.get("alias")
 
         return [FirewallAlias.from_xml(element) for element in element_tree_alias]
+
+    @property
+    def changed(self) -> bool:
+        """
+        some docstring
+        """
+        return self._load_aliases() != self._aliases
+
+    def add_or_update(self, alias: FirewallAlias) -> None:
+        """
+        some docstring.
+        """
+
+        existing_alias: Optional[FirewallAlias] = next(
+            (a for a in self._aliases if a.name == alias.name), None
+        )
+
+        if existing_alias:
+            alias.__dict__.pop("uuid")
+            existing_alias.__dict__.update(alias.__dict__)
+        else:
+            self._aliases.append(alias)
+
+    def delete(self, alias: FirewallAlias) -> bool:
+        """
+        some docstring
+        """
+
+        if alias in self._aliases:
+            self._aliases.remove(alias)
+            return True
+        return False
+
+    def save(self) -> bool:
+        """
+        some doctsring
+        """
+
+        if not self.changed:
+            return False
+
+        filter_element: Element = self._config_xml_tree.find(
+            self._config_maps[self._module_name]["alias"]
+        )
+
+        # Remove specific child elements (e.g., 'alias') from filter_element
+        for alias_element in list(
+            filter_element.findall("alias")
+        ):  # Use list() to avoid modification during iteration
+            filter_element.remove(alias_element)
+
+        # Now, add the updated elements back directly to filter_element
+        filter_element.extend([alias.to_etree() for alias in self._aliases])
+
+        # Write the updated XML tree to the file
+        tree: ElementTree = ElementTree(self._config_xml_tree)
+
+        # raise ValueError([(test.tag, test.text) for test in list(filter_element)[0]])
+
+        tree.write(self._config_path, encoding="utf-8", xml_declaration=True)
+
+        # Reload the configuration to reflect the updated changes
+
+        self._config_xml_tree = self._load_config()
+
+        return True
