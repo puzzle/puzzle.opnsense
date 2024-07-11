2ba3ce439d9c455085bed9041ac9351116ce9cde
KiLLuuuhh <kilian.soltermann@gmx.ch>
2024-07-11 16:43:49 +0200

    fixed pylinter errors

--- tests/unit/plugins/module_utils/test_firewall_alias_utils.py
+++ tests/unit/plugins/module_utils/test_firewall_alias_utils.py
@@ -1,9 +1,11 @@
 #  Copyright: (c) 2024, Puzzle ITC
 #  GNU General Public License v3.0+ (see LICENSE or https://www.gnu.org/licenses/gpl-3.0.txt)
+
 """
 Test suite for the firewall_alias module.
 """
 
+# pylint: disable=duplicate-code
 import os
 from tempfile import NamedTemporaryFile
 from unittest.mock import patch, MagicMock
